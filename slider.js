08816b164cf83632ae580017be2679989ae02131
Mahesh Bhandari <mahibabu1993@gmail.com>
2016-02-19 18:37:57 +0530

    Code shortened

--- slider.js
+++ slider.js
@@ -1,80 +1,71 @@
 var main = function() {
-
-  var int = self.setInterval(function() {
-	$('.arrow-next').trigger('click');
-  }, 3000);
-  
-  $('.slide').hover(function() {
-	clearInterval(int);
-  }, function () {
-	  int = setInterval(function() {
+	var int = self.setInterval(function() {
 		$('.arrow-next').trigger('click');
-	  }, 3000);
-  });
+	}, 3000);
   
-  $('.dot').hover(function() {
-	clearInterval(int);
-  }, function () {
-	  int = setInterval(function() {
-		$('.arrow-next').trigger('click');
-	  }, 3000);
-    });
+	$(".slide, .dot, .arrow-next, .arrow-prev").hover(function() {
+		clearInterval(int);
+	}, function () {
+		int = setInterval(function() {
+			$('.arrow-next').trigger('click');
+		}, 3000);
+	});
 	
-  $('.dot').click(function() {
-	var index = $('.dot').index(this) + 1;
+	$('.dot').click(function() {
+		var index = $('.dot').index(this) + 1;
 	
-	var currentSlide = $('.active-slide');
-	var nextSlide = $('.slide:nth-child('+index+')');
+		var currentSlide = $('.active-slide');
+		var nextSlide = $('.slide:nth-child('+index+')');
 	
-	var currentDot = $('.active-dot');
-	var nextDot = $('.dot:nth-child('+index+')');
+		var currentDot = $('.active-dot');
+		var nextDot = $('.dot:nth-child('+index+')');
 	
-	currentSlide.fadeOut(600).removeClass('active-slide');
-	nextSlide.fadeIn(600).addClass('active-slide');
+		currentSlide.fadeOut(600).removeClass('active-slide');
+		nextSlide.fadeIn(600).addClass('active-slide');
 	
-	currentDot.removeClass('active-dot');
-	nextDot.addClass('active-dot');
-  });
+		currentDot.removeClass('active-dot');
+		nextDot.addClass('active-dot');
+	});
 
   
-  $('.arrow-next').click(function() {
-    var currentSlide = $('.active-slide');
-    var nextSlide = currentSlide.next();
+	$('.arrow-next').click(function() {
+		var currentSlide = $('.active-slide');
+		var nextSlide = currentSlide.next();
 
-    var currentDot = $('.active-dot');
-    var nextDot = currentDot.next();
+		var currentDot = $('.active-dot');
+		var nextDot = currentDot.next();
 
-    if(nextSlide.length === 0) {
-      nextSlide = $('.slide').first();
-      nextDot = $('.dot').first();
-    }
+		if(nextSlide.length === 0) {
+			nextSlide = $('.slide').first();
+			nextDot = $('.dot').first();
+		}
     
-    currentSlide.fadeOut(600).removeClass('active-slide');
-    nextSlide.fadeIn(600).addClass('active-slide');
+		currentSlide.fadeOut(600).removeClass('active-slide');
+		nextSlide.fadeIn(600).addClass('active-slide');
 
-    currentDot.removeClass('active-dot');
-    nextDot.addClass('active-dot');
-  });
+		currentDot.removeClass('active-dot');
+		nextDot.addClass('active-dot');
+	});
 
 
-  $('.arrow-prev').click(function() {
-    var currentSlide = $('.active-slide');
-    var prevSlide = currentSlide.prev();
+	$('.arrow-prev').click(function() {
+		var currentSlide = $('.active-slide');
+		var prevSlide = currentSlide.prev();
 
-    var currentDot = $('.active-dot');
-    var prevDot = currentDot.prev();
+		var currentDot = $('.active-dot');
+		var prevDot = currentDot.prev();
 
-    if(prevSlide.length === 0) {
-      prevSlide = $('.slide').last();
-      prevDot = $('.dot').last();
-    }
+		if(prevSlide.length === 0) {
+			prevSlide = $('.slide').last();
+			prevDot = $('.dot').last();
+		}
     
-    currentSlide.fadeOut(600).removeClass('active-slide');
-    prevSlide.fadeIn(600).addClass('active-slide');
+		currentSlide.fadeOut(600).removeClass('active-slide');
+		prevSlide.fadeIn(600).addClass('active-slide');
 
-    currentDot.removeClass('active-dot');
-    prevDot.addClass('active-dot');
-  });
+		currentDot.removeClass('active-dot');
+		prevDot.addClass('active-dot');
+	});
 
 }
 
